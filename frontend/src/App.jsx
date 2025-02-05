35e6dade09ef7a347872db04d07813e8bd7b15f6
cero96 <m.antoniocero96@hotmail.es>
2025-02-04 19:56:57 -0500

    Creacion de Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,16 @@
-// frontend/src/App.jsx
+// App.jsx
 
-import { useState } from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Formulario } from './components/Formulario';
-import { Home } from './components/Home';
+import React from 'react';
+import Home from './components/Home';  // Asegúrate de que el path sea correcto
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-  const [user, setUser] = useState(null);
-
-  const handleLogin = async (email, password) => {
-    try {
-      const response = await axios.post(import.meta.env.VITE_URL + '/login', { email, password });
-      setUser(response.data.user);  // Guardamos el usuario en el estado
-      localStorage.setItem('token', response.data.token);  // Guardamos el token en localStorage
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error);
-    }
-  };
-
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Formulario onLogin={handleLogin} />} />
-        <Route path="/home" element={<Home user={user} setUser={setUser} />} />
-      </Routes>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
-};
+}
 
 export default App;
