a746298e2542ab8e35b2c3bf5e479458b8d12efa
bmcabrera1 <bcabrera2195@gmail.com>
2025-02-14 15:24:46 -0500

    Ajustes en el login

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,23 +1,37 @@
-// /src/components/Auth.jsx
-
-import React, { useState } from 'react';
-import Login from './Login';  // Asegúrate de que esto apunta al componente Login correctamente
-import Register from './Register';  // Asegúrate de que esto apunta al componente Register correctamente
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
 
 const Auth = () => {
+  const [activeTab, setActiveTab] = useState("login");
+
   return (
-    <div className="container d-flex justify-content-center align-items-center min-vh-100">
-      <div className="d-flex justify-content-between w-100">
-        <div style={{ width: '45%' }}>
-          <Login />
-        </div>
-        <div style={{ width: '45%' }}>
-          <Register />
-        </div>
+    <div className="container d-flex flex-column align-items-center min-vh-100">
+      <div className="btn-group w-100 mb-4 d-flex">
+        <button
+          className={`btn ${
+            activeTab === "login" ? "btn-primary" : "btn-outline-primary"
+          } w-50`}
+          onClick={() => setActiveTab("login")}
+        >
+          Login
+        </button>
+        <button
+          className={`btn ${
+            activeTab === "register" ? "btn-primary" : "btn-outline-primary"
+          } w-50`}
+          onClick={() => setActiveTab("register")}
+        >
+          Register
+        </button>
+      </div>
+
+      <div className="w-100 d-flex justify-content-center">
+        {activeTab === "login" ? <Login /> : <Register />}
       </div>
     </div>
   );
 };
 
 export default Auth;
-
