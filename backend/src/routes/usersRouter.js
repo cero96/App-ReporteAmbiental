7fe03a7c598b2058cf5dd18d8640ec366f832025
cero96 <m.antoniocero96@hotmail.es>
2025-02-14 00:29:37 -0500

    manejo de usuarios

--- backend/src/routes/usersRouter.js
+++ backend/src/routes/usersRouter.js
@@ -1,53 +1,17 @@
 import { Router } from "express";
-import { body, param } from "express-validator";
-import { handleInputErrors } from "../middleware/validations.js";
 import { UsersController } from "../controllers/UsersController.js";
-import User from "../models/users.js";
+import { authenticateToken } from "../middleware/authMiddleware.js"; // Importa el middleware
 
 const router = Router();
 
-// Rutas para usuarios
-router.get("/", UsersController.getAll);
-router.get("/:id", UsersController.getById);
-router.post(
-  "/",
-  body("name").notEmpty().withMessage("El nombre no puede ir vacío").toUpperCase(),
-  body("email")
-    .notEmpty()
-    .withMessage("El correo no puede ir vacío")
-    .isEmail()
-    .withMessage("El correo ingresado no es válido")
-    .toLowerCase()
-    .custom(async (value) => {
-      const user = await User.findOne({ where: { email: value } });
-      if (user) {
-        throw new Error("El correo electrónico ya está registrado");
-      }
-      return true;
-    }),
-  body("password")
-    .notEmpty()
-    .withMessage("La contraseña no puede ir vacía")
-    .isLength({ min: 8 })
-    .withMessage("La contraseña debe tener al menos 8 caracteres"),
-  handleInputErrors,
-  UsersController.create
-);
-router.put("/:id", UsersController.updateById);
-router.delete("/:id", UsersController.deleteById);
+// Rutas sin protección
+router.post("/login", UsersController.login);
+router.post("/", UsersController.create);
 
-router.get(
-  "/:id",
-  param("id")
-    .isInt()
-    .withMessage("Id no valido")
-    .custom((value) => value > 0)
-    .withMessage("Id no valido"),
-  handleInputErrors,
-  UsersControllers.getById
-);
-
-router.put("/:id", UsersControllers.updateById);
-router.delete("/:id", UsersControllers.deleteById);
+// Rutas protegidas
+router.get("/", authenticateToken, UsersController.getAll);
+router.get("/:id", authenticateToken, UsersController.getById);
+router.put("/:id", authenticateToken, UsersController.updateById);
+router.delete("/:id", authenticateToken, UsersController.deleteById);
 
 export default router;
